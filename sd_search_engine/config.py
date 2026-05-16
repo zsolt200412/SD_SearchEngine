96e265f7fd2b6fe0205eef2ca5a59a0e64cdb0aa
zsolt <kolumbanzsolt2004@gmail.com>
2026-05-16 12:04:54 +0300

    New file extensions.

--- sd_search_engine/config.py
+++ sd_search_engine/config.py
@@ -1,6 +1,7 @@
-ignored_extensions = {".exe", ".dll", ".bin", ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".pdf", ".zip", ".tar", ".gz",
+ignored_extensions = {".exe", ".dll", ".bin", ".pdf", ".zip", ".tar", ".gz",
                       ".7z", ".mp3", ".mp4", ".avi", ".mkv", ".iso", ".dmg", ".apk", ".app", ".msi", ".deb", ".rpm", ".vhd", ".vmdk"}
 ignored_folders = {"node_modules", ".git", "__pycache__", "venv", ".venv"}
 text_extensions = {".txt", ".md", ".py", ".java", ".c", ".cpp", ".js", ".html", ".css", 
                    ".json", ".xml", ".csv", ".log", ".yaml", ".yml", ".h", ".hpp", ".go",
                      ".rs", ".sh", ".bat", ".ps1", ".php", ".rb", ".swift", ".kt", ".kts"}
+image_extensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".ico", ".tiff", ".svg"}
